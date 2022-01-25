d49f7b9170df9a2bb4e9aca4798cffce2e8855d4
Nadher Arroum <nadher.arroum@gmail.com>
2022-01-25 17:45:55 +0100

    Design Layout

---
+++ app/src/main/java/com/nadherarroum/currency_converter/MainActivity.java
@@ -0,0 +1,19 @@
+package com.nadherarroum.currency_converter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+
+        setContentView(R.layout.activity_main);
+    }
+}
